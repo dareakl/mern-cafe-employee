a7c1ec21c68ac1d17cc2a502192c99d27fa3af1c
Sathish Raja <msathis.akl@hotmail.com>
2024-09-30 03:36:07 +0800

    code cleaning and readme added

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # mern-cafe-employee
+
+1.To start Server
+
+go to backend folder
+
+        npm i or npm install
+        After installation done
+        npm start
+
+
+2.To start front end
+
+    go to frontend folder
+
+        npm i or npm install
+        After installation done
+        npm start
+
+3.To do unit test
+
+    go to backend folder
+        npm test
--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-
-// dotenv.config();
-
-// const app = express();
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// const cafeRoutes = require("./routes/cafe");
-// const employeeRoutes = require("./routes/employee");
-
-// app.use("/cafe", cafeRoutes);
-// app.use("/employee", employeeRoutes);
-
-// const PORT = process.env.PORT || 4000;
-
-// mongoose
-//   .connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//   })
-//   .catch((err) => console.log(err));
-
-// module.exports = app; // Export the app
-
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -45,21 +13,13 @@ app.use(bodyParser.json());
 const cafeRoutes = require("./routes/cafe");
 const employeeRoutes = require("./routes/employee");
 
-app.use("/cafes", cafeRoutes); // Note the path should be /cafes
+app.use("/cafe", cafeRoutes);
 app.use("/employee", employeeRoutes);
 
 const PORT = process.env.PORT || 4000;
 
-// Check if the NODE_ENV is set to 'test'
-const isTestEnv = process.env.NODE_ENV === "test";
-
-// Use a different database URI for tests
-const mongoUri = isTestEnv
-  ? process.env.TEST_MONGODB_URI
-  : process.env.MONGODB_URI;
-
 mongoose
-  .connect(mongoUri, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
